a5a7cf46092fa8af74b62c6ee10e4954879785fd
thewickedwench18 <jdwh251@uky.edu>
2026-05-09 22:41:15 -0400

    Create getmyapps.sh
    yeah

---
+++ getmyapps.sh
@@ -0,0 +1,23 @@
+# Author:Veronica Whitaker 
+# or thewickedwench18 on everything
+# Hi this is my code to install all of my preffered apps on a new computer just in case oyu want to try a new OS or
+# have a new computer
+# This will just download the installers for steam, firefox, spotify
+# might be neat
+# specifically this is an executable in your terminal that curls the urls of each installer
+# since the installer changes constantly it'll probably not be able to run it normally, you gotta do that yourself
+# ciao
+# also this is written in bash so that linux and most terminals will be able to run it easily, mainly linux tho
+#
+#
+# written 05/09/2026
+# curl multiple urls 
+# 
+
+
+
+sudo apt install steam
+sudo snap install spotify
+
+
+
